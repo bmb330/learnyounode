e99c03fc42cdbc05e017c6edc14de1dc214bae37
Brandon Braun <brandon.braun@gmail.com>
2015-12-10 18:44:13 -0500

    complete exercise 12, add post check

--- ex12.js
+++ ex12.js
@@ -4,6 +4,9 @@ var map = require('through2-map');
 var port = process.argv[2];
 
 var server = http.createServer(function(req, res) {
+  if (req.method != 'POST') {
+    return res.end('send me a POST\n');
+  }
   req.pipe(map(function (data) {
     return data.toString().toUpperCase();
   })).pipe(res);
